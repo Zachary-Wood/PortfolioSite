f9970133e1ad1d833a523367e0934c31df1f92b2
Zach Wood <zacharydavwood@gmail.com>
2025-01-24 19:58:38 -0500

    Added layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+// added global layouts
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
